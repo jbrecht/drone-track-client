509574b494f0d358f2a1aa57968103001ade5501
John Brecht <jbrecht@rosettastone.com>
2015-04-21 10:00:40 -0700

    real implementation
    added real map and interactivity implementation

--- drone_track.js
+++ drone_track.js
@@ -1,7 +1,58 @@
-var socket = io('http://0.0.0.0:1337');
+$(document).ready(function() {
 
-socket.emit('status_update', 'flying');
+  var map = L.map('map');
+  var status;
 
-socket.on('timer_tick', function(data){
-  console.log('timer_tick: ', data);
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'examples.map-i875mjb7'
+  }).addTo(map);
+
+  var getStatus = function() {
+    if(status) {
+      return ''+status;
+    } else {
+      return '';
+    }
+  };
+
+  var marker = L.circle([0, 0], 2, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+  }).addTo(map);
+
+
+  var socket = io('http://0.0.0.0:1337');
+
+  $('#fly').button().click(function() {
+    socket.emit('status_update', 'flying');
+  });
+  $('#pause').button().click(function() {
+    socket.emit('status_update', 'paused');
+  });
+  $('#reset').button().click(function() {
+    socket.emit('status_update', 'reset');
+  });
+
+  socket.on('timer_tick', function(data){
+    status = data;
+    marker.setLatLng(  L.latLng(data.position[0], data.position[1]));
+  });
+
+  socket.on('footprint', function(data){
+    var footprint = data.footprint;
+    console.log('footprint: ', footprint);
+    var bounds = [
+      [footprint[0], footprint[1]],
+      [footprint[2], footprint[3]]
+    ];
+    console.log('bounds: ', bounds);
+    map.fitBounds(bounds);
+  });
+
+
 });
